4f1913d9d1867a0fa49910ff28bab3583548d32b
dthrottle <ted.donald@gmail.com>
2023-05-18 05:56:27 +0000

    add initial feedback items

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
+import { useState } from 'react'
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData'
 
 function App() {
+
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const handleClick = () => {
+    setFeedback({})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <FeedbackList onclick={handleClick} feedback={feedback}/>
     </div>
   );
 }
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem onsectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'ipsum adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ]
+  
+  export default FeedbackData
